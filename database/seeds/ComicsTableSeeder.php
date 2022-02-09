b97758fba9dfe5123e3d1e4fe3f29660b7d3baa8
Andrea <57769818+andrea-nuzzo@users.noreply.github.com>
2022-02-09 15:47:44 +0100

    fixed bug seeder

--- database/seeds/ComicsTableSeeder.php
+++ database/seeds/ComicsTableSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
-
+use App\Comic;
 class ComicsTableSeeder extends Seeder
 {
     /**
@@ -11,6 +11,21 @@ class ComicsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+         //Richiamo i dati dal file comics.php all'interno della cartella config
+         $comics = config("comics");
+
+         // Ciclo per popolare la tabella del mio DB
+         foreach($comics as $comic){
+             $newComic = new Comic();
+             $newComic->title = $comic["title"];
+             $newComic->description = $comic["description"];
+             $newComic->thumb = $comic["thumb"];
+             $newComic->price = $comic["price"];
+             $newComic->series = $comic["series"];
+             $newComic->sale_date = $comic["sale_date"];
+             $newComic->type = $comic["type"];
+             $newComic->save();
+ 
+         }
     }
 }
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Database\Seeder;
-use App\Comic;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,21 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        //Richiamo i dati dal file comics.php all'interno della cartella config
-        $comics = config("comics");
-
-        // Ciclo per popolare la tabella del mio DB
-        foreach($comics as $comic){
-            $newComic = new Comic();
-            $newComic->title = $comic["title"];
-            $newComic->description = $comic["description"];
-            $newComic->thumb = $comic["thumb"];
-            $newComic->price = $comic["price"];
-            $newComic->series = $comic["series"];
-            $newComic->sale_date = $comic["sale_date"];
-            $newComic->type = $comic["type"];
-            $newComic->save();
-
-        }
+        // $this->call(UserSeeder::class);
     }
 }
